fc2a4ea2b684e2c2798352f2f343ab65b38783d9
hervin21 <142458280+hervin21@users.noreply.github.com>
2023-10-02 19:21:43 -0600

    Agregar archivos de proyecto.

---
+++ ejemplo de arreglos/ejemplo de arreglos.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+int main() {
+	int numero[6] = { 10, 20, 30, 40, 50, 60 };
+	std::cout << numero[1] << std::endl;
+
+	int n[] = { 3, 4, 5 };
+	std::cout << n[0] << std::endl;
+	std::cout << n[1] << std::endl;
+	std::cout << n[2] << std::endl;
+
+	char c[4] = { 'L' , 'U', 'I','S' };
+	std::cout << c[0] << std::endl;
+	std::cout << c[1] << std::endl;
+
+	char s[] = { "mona lisa" };
+	std::cout << s << std::endl;
+
+	return 0;
+}
